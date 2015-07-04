4947b6884161ec3034cd7650e11aaf3abed7ea36
Jeffrey Hing <hingcruz@gmail.com>
2015-07-03 19:56:21 -0700

    FIx JSON.stringify cyclical dependency error

--- dist/extendThis.js
+++ dist/extendThis.js
@@ -670,7 +670,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return typeof value === 'function';
 	}
 
-	/**
+	/*
 	 * Copies the keys in the source object to the sourceKey object.
 	 */
 	function extendSourceKeys(sourceKeys, source, regexp) {
@@ -679,6 +679,22 @@ return /******/ (function(modules) { // webpackBootstrap
 	            sourceKeys[key] = key;
 	        }
 	    }
+	}
+
+	/*
+	 * Removes cyclical dependencies.
+	 */
+	function stringify(object) {
+	    var seen = [];
+	    return JSON.stringify(object, function(key, val) {
+	        if (val !== null && typeof val === 'object') {
+	            if (seen.indexOf(val) >= 0) {
+	                return undefined;
+	            }
+	            seen.push(val);
+	        }
+	        return val;
+	    });
 	}
 
 	//--------------------------------------
@@ -694,20 +710,20 @@ return /******/ (function(modules) { // webpackBootstrap
 	    };
 
 	    function illegalArgument(arg, message) {
-	        var full = JSON.stringify(arg) + ': ' + message;
+	        var full = stringify(arg) + ': ' + message;
 	        throw new Error(formatMessage('Illegal argument', full));
 	    }
 
 	    function propertyNotFound(key, object) {
 	        if (config.throwPropertyNotFoundError) {
-	            var message = key + ' in ' + JSON.stringify(object);
+	            var message = key + ' in ' + stringify(object);
 	            throw new Error(formatMessage('Property not found', message));
 	        }
 	    }
 
 	    function propertyOverride(key, object) {
 	        if (config.throwOverrideError) {
-	            var message = key + ' in ' + JSON.stringify(object);
+	            var message = key + ' in ' + stringify(object);
 	            throw new Error(formatMessage('Property already exists',
 	                message));
 	        }
@@ -715,7 +731,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    function formatMessage(name, message) {
 	        if (!isString(message)) {
-	            message = JSON.stringify(message);
+	            message = stringify(message);
 	        }
 	        return config.filename + ': ' + name + ': ' + message;
 	    }
